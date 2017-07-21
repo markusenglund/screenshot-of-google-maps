4d051c6b5bccd9c0933d06884b3702de18e58a8a
yogaboll <markus.s.englund@gmail.com>
2017-07-21 18:44:47 +0700

    add ability to download screenshot as image

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from "react"
 import ReactDOM from "react-dom"
-
+import DownloadButton from "./components/DownloadButton"
 import "./styles.css"
-
-// import Home from "./components/Home"
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Back in business boys</h1>
+        <DownloadButton />
       </div>
     )
   }
---
+++ src/components/DownloadButton.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { saveAs } from "file-saver"
+import html2canvas from "html2canvas"
+
+function handleImageDownload() {
+// Insane workaround: Set new css-values on map-element so html2canvas can properly process the map
+// Get transform values
+//   const element = document.querySelector(".gm-style div div")
+//   const transform = element.style.transform
+//   const splitMatrix = transform.split(",")
+//   const left = parseFloat(splitMatrix[4]) // get left value
+//   const top = parseFloat(splitMatrix[5])  // get top value
+//
+//   // Set top- and left-values
+//   element.style.transform = "none"
+//   element.style.left = `${left}px`
+//   element.style.top = `${top}px`
+//
+  // Turn google maps into canvas and download canvas as png
+  html2canvas(
+    document.querySelector("#map"),
+    { useCORS: true }
+  ).then((canvas) => {
+    canvas.toBlob((blob) => {
+      saveAs(blob, "map.png")
+    })
+    document.body.appendChild(canvas)
+    // Put element back together
+    // element.style.transform = transform
+    // element.style.left = "0px"
+    // element.style.top = "0px"
+  })
+}
+
+function DownloadButton() {
+  return (
+    <div id="download-button-wrapper">
+      <button id="download-button" onClick={handleImageDownload}>
+        {/* <i className="fa fa-download" aria-hidden /> */}
+        Download
+      </button>
+    </div>
+  )
+}
+
+export default DownloadButton
