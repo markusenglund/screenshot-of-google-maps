a10cd815553b379533806fb48b07980f52148c2e
yogaboll <markus.s.englund@gmail.com>
2017-07-21 23:57:01 +0700

    improve map start position

--- public/map.js
+++ public/map.js
@@ -2,7 +2,8 @@ let map
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 0, lng: 0 },
-    zoom: 3,
+    zoom: 2,
+    center: { lat: 25, lng: 0 },
     streetViewControl: false,
     mapTypeControlOptions: {
       // style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
